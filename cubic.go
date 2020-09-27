cef5035e95ec24c25030c8c8ece431144cbbcd6d
Tony Rippy <tonyrippy@gmail.com>
2020-09-27 09:14:45 -0400

    Added method to compute tangents of monotonic splines.

--- cubic.go
+++ cubic.go
@@ -1,6 +1,10 @@
-// Methods for performing cubic interpolation. 
+// Methods for performing cubic interpolation.
 
 package math
+
+import (
+	"math"
+)
 
 /*
   Returns a closure that performs cubic interpolation.
@@ -17,7 +21,7 @@ func cubic6(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 {
 		tt := t * t
 		ttt := tt * t
 		return (
-			(2*ttt - 3*tt + 1) * y1 + 
+			(2*ttt - 3*tt + 1) * y1 +
 			(ttt - 2*tt + t) * dy1 * dx +
 			(-2*ttt + 3*tt) * y2 +
 			(ttt - tt) * dy2 * dx)
@@ -27,16 +31,16 @@ func cubic6(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 {
 /*
   A cubic spline can be expressed as a third-degree polynomial function:
     f(x) = Ax^3 + Bx^2 + Cx + D
-  
+
   This function, given the values of x, f(x) and f'(x) at two end points,
-  will calcuate and return the values for A, B, C, D.     
+  will calcuate and return the values for A, B, C, D.
 */
 func SolveCubic(x1, y1, dy1, x2, y2, dy2 float64) (A, B, C, D float64) {
 	// Yeah, sorry about this mess... I will try and post a derivation for this.
 	dx := x2 - x1
 	xx1 := x1 * x1
 	xx2 := x2 * x2
-	
+
 	a1 := (xx2 * x2) - (xx1 * x1)
 	a2 := a1
 	a1 -= 3 * xx1 * dx
@@ -64,10 +68,10 @@ func SolveCubic(x1, y1, dy1, x2, y2, dy2 float64) (A, B, C, D float64) {
 /*
   Returns a closure that performs cubic interpolation.
   This returns a more efficient closure that is expensive to create,
-  but minimizes runtime computation. 
+  but minimizes runtime computation.
   Only requires the capture of 4 float64 values.
 */
-func cubic4(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 { 
+func cubic4(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 {
 	A, B, C, D := SolveCubic(x1, y1, dy1, x2, y2, dy2)
 	return func(x float64) float64 {
 		xx := x * x
@@ -82,6 +86,63 @@ func cubic4(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 {
   a function f(x) that allows one to interpolate values between the two
   given points.
 */
-func CubicFunction(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 { 
+func CubicFunction(x1, y1, dy1, x2, y2, dy2 float64) func (float64) float64 {
 	return cubic4(x1, y1, dy1, x2, y2, dy2)
+}
+
+/*
+FritschCarlsonTangents calculates tangents for a set of points
+that ensure monotonicity for a resulting Hermite spline.
+
+This function makes some important assumptions:
+  1. That the input arrays have the same length.
+  2. That the data points are monotonic.
+  3. That the input points are sorted on the x axis, ascending.
+These assumptions are not verified by the method.
+*/
+func FritschCarlsonTangents(xs, ys []float64) []float64 {
+	// For implementation details, see:
+	// https://en.wikipedia.org/wiki/Monotone_cubic_interpolation
+	n := len(xs)
+	if n == 0 {
+		return []float64{}
+	}
+	if n == 1 {
+		return []float64{0}
+	}
+	// Compute the slopes of the secant lines between successive points
+	d := make([]float64, n-1)
+	for i := 0; i < n-1; i++ {
+		d[i] = (ys[i+1] - ys[i]) / (xs[i+1] - xs[i])
+	}
+	// Compute provisional tangents
+	m := make([]float64, n)
+	m[0] = d[0]
+	m[n-1] = d[n-2]
+	for i := 1; i < n-1; i++ {
+		if d[i] == 0.0 {
+			m[i] = 0.0
+			i += 1
+			m[i] = 0.0
+			continue
+		}
+		if math.Signbit(d[i-1]) != math.Signbit(d[i]) {
+			m[i] = 0.0
+		} else {
+			m[i] = (d[i-1] + d[i]) / 2
+		}
+	}
+	// Adjust tangents to keep monoticity.
+	for i := 0; i < n-1; i++ {
+		dk := d[i]
+		ak := m[i] / dk
+		bk := m[i+1] / dk
+		sqsum := ak*ak + bk*bk
+		if sqsum > 9.0 {
+			tk := 3.0 / math.Sqrt(sqsum)
+			m[i] = tk * ak * dk
+			m[i+1] = tk * bk * dk
+		}
+	}
+	return m
 }
--- cubic_test.go
+++ cubic_test.go
@@ -37,3 +37,53 @@ func TestCubic4vs6(t *testing.T) {
 		})
 	}
 }
+
+func TestMonoticity(t *testing.T) {
+	cases := []struct{
+		Name string
+		Xs, Ys []float64
+	}{
+		{
+			"Flat",
+			[]float64{0, 10, 20, 30, 60},
+			[]float64{3, 3, 3, 3, 3},
+		},
+		{
+			"SmoothIncrease",
+			[]float64{0, 1, 2, 3, 4},
+			[]float64{2, 4, 6, 8, 10},
+		},
+		{
+			"Curve",
+			[]float64{0, 1, 2, 5, 60},
+			[]float64{0, .1, .333, .667, .8},
+		},
+		{
+			"SharpCurve",
+			[]float64{0, 1, 2, 3, 4},
+			[]float64{0, .1, .9, .95, .99},
+		},
+	}
+	const pointsToCheck = 32
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			ms := FritschCarlsonTangents(tc.Xs, tc.Ys)
+			n := len(ms)
+			for i:= 0; i < n-1; i++ {
+				minx := tc.Xs[i]
+				maxx := tc.Xs[i+1]
+				f := cubic4(minx, tc.Ys[i], ms[i], maxx, tc.Ys[i+1], ms[i+1])
+				dx := (maxx - minx) / pointsToCheck
+				oy := tc.Ys[i]
+				for x := minx + dx; x < maxx; x += dx {
+					y := f(x)
+					if y < oy {
+						t.Errorf("Not monotonic: f(%f) = %f > f(%f) = %f", x - dx, oy, x, y)
+						break
+					}
+					oy = y
+				}
+			}
+		})
+	}
+}
